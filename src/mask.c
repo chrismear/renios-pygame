6ea9ecec4f45341412c2863547a577f8d11219da
Chris Mear <chris@feedmechocolate.com>
2013-01-02 19:01:16 +0000

    Teach mask.c to find colorkey.

--- src/mask.c
+++ src/mask.c
@@ -39,6 +39,38 @@
 #define M_PI 3.14159265358979323846
 #endif
 
+typedef struct
+{
+    Uint8 *src;
+    int src_w, src_h;
+    int src_pitch;
+    int src_skip;
+    Uint8 *dst;
+    int dst_w, dst_h;
+    int dst_pitch;
+    int dst_skip;
+    SDL_PixelFormat *src_fmt;
+    SDL_PixelFormat *dst_fmt;
+    Uint8 *table;
+    int flags;
+    Uint32 colorkey;
+    Uint8 r, g, b, a;
+} SDL_BlitInfo;
+
+typedef struct SDL_BlitMap
+{
+    SDL_Surface *dst;
+    int identity;
+    SDL_blit blit;
+    void *data;
+    SDL_BlitInfo info;
+
+    /* the version count matches the destination; mismatch indicates
+       an invalid mapping */
+    Uint32 dst_palette_version;
+    Uint32 src_palette_version;
+} SDL_BlitMap;
+
 static PyTypeObject PyMask_Type;
 
 /* mask object methods */
@@ -527,7 +559,7 @@ static PyObject* mask_from_surface(PyObject* self, PyObject* args)
                 }
             } else {
                 /*  test against the colour key. */
-                if (format->colorkey != color) {
+                if (surf->map->info.colorkey != color) {
                     bitmask_setbit(mask, x, y);
                 }
             }
