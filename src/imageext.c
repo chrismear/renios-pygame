2b7a48400234d7f25566e73cd95d3c0eed514030
Chris Mear <chris@feedmechocolate.com>
2013-01-03 09:48:04 +0000

    Teach imageext.c how to find colorkey and alpha under SDL2.

--- src/imageext.c
+++ src/imageext.c
@@ -50,6 +50,38 @@
 #include "pgopengl.h"
 #include <SDL_image.h>
 
+typedef struct
+{
+    Uint8 *src;
+    int src_w, src_h;
+    int src_pitch;
+    int src_skip;
+    Uint8 *dst;
+    int dst_w, dst_h;
+    int dst_pitch;
+    int dst_skip;
+    SDL_PixelFormat *src_fmt;
+    SDL_PixelFormat *dst_fmt;
+    Uint8 *table;
+    int flags;
+    Uint32 colorkey;
+    Uint8 r, g, b, a;
+} SDL_BlitInfo;
+
+typedef struct SDL_BlitMap
+{
+    SDL_Surface *dst;
+    int identity;
+    SDL_blit blit;
+    void *data;
+    SDL_BlitInfo info;
+
+    /* the version count matches the destination; mismatch indicates
+       an invalid mapping */
+    Uint32 dst_palette_version;
+    Uint32 src_palette_version;
+} SDL_BlitMap;
+
 static char*
 find_extension (char* fullname)
 {
@@ -249,11 +281,11 @@ SavePNG (SDL_Surface *surface, char *file)
         return -1;
 
     surf_flags = surface->flags & (SDL_SRCALPHA | SDL_SRCCOLORKEY);
-    surf_alpha = surface->format->alpha;
+    surf_alpha = surface->map->info.a;
     if (surf_flags & SDL_SRCALPHA)
         SDL_SetAlpha (surface, 0, 255);
     if (surf_flags & SDL_SRCCOLORKEY)
-        SDL_SetColorKey (surface, 0, surface->format->colorkey);
+        SDL_SetColorKey (surface, 0, surface->map->info.colorkey);
 
     ss_rect.x = 0;
     ss_rect.y = 0;
@@ -271,7 +303,7 @@ SavePNG (SDL_Surface *surface, char *file)
     if (surf_flags & SDL_SRCALPHA)
         SDL_SetAlpha (surface, SDL_SRCALPHA, (Uint8)surf_alpha);
     if (surf_flags & SDL_SRCCOLORKEY)
-        SDL_SetColorKey (surface, SDL_SRCCOLORKEY, surface->format->colorkey);
+        SDL_SetColorKey (surface, SDL_SRCCOLORKEY, surface->map->info.colorkey);
 
     for (i = 0; i < ss_h; i++)
     {
